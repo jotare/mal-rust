921dcf43768a3f9621cd54e338cf6f1bf4408d80
Joan Antoni RE <joanantoni.re16@gmail.com>
2022-03-10 17:02:34 +0100

    Update tests to step 1 (only mandatory)

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,49 +1,45 @@
 use mal_rust;
 
 #[test]
-fn testing_basic_string() {
-    assert_eq!(mal_rust::rep("abcABC123"), "abcABC123");
+fn testing_read_of_numbers() {
+    assert_eq!(mal_rust::rep("1"), "1");
+    assert_eq!(mal_rust::rep("7"), "7");
+    assert_eq!(mal_rust::rep("7"), "7");
+    assert_eq!(mal_rust::rep("-123"), "-123");
 }
 
 #[test]
-fn testing_string_containing_spaces() {
-    assert_eq!(mal_rust::rep("hello mal world"), "hello mal world");
-}
-
-#[test]
-fn testing_string_containing_symbols() {
-    assert_eq!(mal_rust::rep("[]{}\"'* ;:()"), "[]{}\"'* ;:()");
-}
-
-#[test]
-fn test_long_string() {
-    assert_eq!(mal_rust::rep("hello world abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789 (;:() []{}\"'* ;:() []{}\"'* ;:() []{}\"'*)"), "hello world abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789 (;:() []{}\"'* ;:() []{}\"'* ;:() []{}\"'*)");
-}
-
-#[test]
-fn non_alphanumeric_characters() {
-    assert_eq!(mal_rust::rep("!"), "!");
-    assert_eq!(mal_rust::rep("&"), "&");
+fn testing_read_of_symbols() {
     assert_eq!(mal_rust::rep("+"), "+");
-    assert_eq!(mal_rust::rep(","), ",");
-    assert_eq!(mal_rust::rep("-"), "-");
-    assert_eq!(mal_rust::rep("/"), "/");
-    assert_eq!(mal_rust::rep("<"), "<");
-    assert_eq!(mal_rust::rep("="), "=");
-    assert_eq!(mal_rust::rep(">"), ">");
-    assert_eq!(mal_rust::rep("?"), "?");
-    assert_eq!(mal_rust::rep("@"), "@");
-    assert_eq!(mal_rust::rep("^"), "^");
-    assert_eq!(mal_rust::rep("_"), "_");
-    assert_eq!(mal_rust::rep("`"), "`");
-    assert_eq!(mal_rust::rep("~"), "~");
+    assert_eq!(mal_rust::rep("abc"), "abc");
+    assert_eq!(mal_rust::rep("abc"), "abc");
+    assert_eq!(mal_rust::rep("abc5"), "abc5");
+    assert_eq!(mal_rust::rep("abc-def"), "abc-def");
 }
 
 #[test]
-fn non_alphanumeric_characters_2() {
-    assert_eq!(mal_rust::rep("#"), "#");
-    assert_eq!(mal_rust::rep("$"), "$");
-    assert_eq!(mal_rust::rep("%"), "%");
-    assert_eq!(mal_rust::rep("."), ".");
-    assert_eq!(mal_rust::rep("|"), "|");
+fn testing_non_numbers_starting_with_a_dash() {
+    assert_eq!(mal_rust::rep("-"), "-");
+    assert_eq!(mal_rust::rep("-abc"), "-abc");
+    assert_eq!(mal_rust::rep("->>"), "->>");
+}
+
+#[test]
+fn testing_read_of_lists() {
+    assert_eq!(mal_rust::rep("(+ 1 2)"), "(+ 1 2)");
+    assert_eq!(mal_rust::rep("()"), "()");
+    assert_eq!(mal_rust::rep("( )"), "()");
+    assert_eq!(mal_rust::rep("(nil)"), "(nil)");
+    assert_eq!(mal_rust::rep("((3 4))"), "((3 4))");
+    assert_eq!(mal_rust::rep("(+ 1 (+ 2 3))"), "(+ 1 (+ 2 3))");
+    assert_eq!(mal_rust::rep("( +   1   (+   2 3   )   )"), "(+ 1 (+ 2 3))");
+    assert_eq!(mal_rust::rep("(* 1 2)"), "(* 1 2)");
+    assert_eq!(mal_rust::rep("(** 1 2)"), "(** 1 2)");
+    assert_eq!(mal_rust::rep("(* -3 6)"), "(* -3 6)");
+    assert_eq!(mal_rust::rep("(()())"), "(() ())");
+}
+
+#[test]
+fn test_commas_as_whitespace() {
+    assert_eq!(mal_rust::rep("(1 2, 3,,,,),,"), "(1 2 3)");
 }
