611ae4d90ea8f5b60fa6f788660a0333bdc55187
Joan Antoni RE <joanantoni.re16@gmail.com>
2022-03-10 17:02:23 +0100

    Update REP functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
-fn read(input: &str) -> &str {
-    input
+mod printer;
+mod reader;
+mod types;
+
+use crate::types::Ast;
+
+
+fn read(input: &str) -> Ast {
+    reader::read_str(input)
 }
 
-fn eval(input: &str) -> &str {
-    input
+fn eval(ast: Ast) -> Ast {
+    ast
 }
 
-fn print(input: &str) -> &str {
-    input
+fn print(ast: Ast) -> String {
+    printer::pr_str(ast)
 }
 
-pub fn rep(input: &str) -> &str {
+pub fn rep(input: &str) -> String {
     print(eval(read(input)))
 }
